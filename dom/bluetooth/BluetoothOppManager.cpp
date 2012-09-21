f191466eec3756bb261f74391992ca61b04b6b7b
Eric Chou <echou@mozilla.com>
2012-09-21 21:09:07 +0800

    Bug 792823 - patch 2: BluetoothOppManager interface and prototype

---
+++ dom/bluetooth/BluetoothOppManager.cpp
@@ -0,0 +1,102 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#include "BluetoothOppManager.h"
+
+#include "BluetoothReplyRunnable.h"
+#include "BluetoothService.h"
+#include "BluetoothServiceUuid.h"
+
+USING_BLUETOOTH_NAMESPACE
+
+static BluetoothOppManager* sInstance = nullptr;
+
+BluetoothOppManager::BluetoothOppManager()
+{
+}
+
+BluetoothOppManager::~BluetoothOppManager()
+{
+}
+
+//static
+BluetoothOppManager*
+BluetoothOppManager::Get()
+{
+  if (sInstance == nullptr) {
+    sInstance = new BluetoothOppManager();
+  }
+
+  return sInstance;
+}
+
+bool
+BluetoothOppManager::Connect(const nsAString& aDeviceObjectPath,
+                             BluetoothReplyRunnable* aRunnable)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  BluetoothService* bs = BluetoothService::Get();
+  if (!bs) {
+    NS_WARNING("BluetoothService not available!");
+    return false;
+  }
+
+  nsString serviceUuidStr =
+    NS_ConvertUTF8toUTF16(mozilla::dom::bluetooth::BluetoothServiceUuidStr::ObjectPush);
+
+  nsRefPtr<BluetoothReplyRunnable> runnable = aRunnable;
+
+  nsresult rv = bs->GetSocketViaService(aDeviceObjectPath,
+                                        serviceUuidStr,
+                                        BluetoothSocketType::RFCOMM,
+                                        true,
+                                        false,
+                                        this,
+                                        runnable);
+
+  runnable.forget();
+  return NS_FAILED(rv) ? false : true;
+}
+
+bool
+BluetoothOppManager::Disconnect(BluetoothReplyRunnable* aRunnable)
+{
+  MOZ_ASSERT(NS_IsMainThread());
+
+  BluetoothService* bs = BluetoothService::Get();
+  if (!bs) {
+    NS_WARNING("BluetoothService not available!");
+    return false;
+  }
+
+  nsRefPtr<BluetoothReplyRunnable> runnable = aRunnable;
+
+  nsresult rv = bs->CloseSocket(this, runnable);
+  runnable.forget();
+
+  return NS_FAILED(rv) ? false : true;
+}
+
+bool
+BluetoothOppManager::SendFile(const nsAString& aFileUri,
+                              BluetoothReplyRunnable* aRunnable)
+{
+  return true;
+}
+
+bool
+BluetoothOppManager::StopSendingFile(BluetoothReplyRunnable* aRunnable)
+{
+  return true;
+}
+
+// Virtual function of class SocketConsumer
+void
+BluetoothOppManager::ReceiveSocketData(mozilla::ipc::SocketRawData* aMessage)
+{
+}
+
---
+++ dom/bluetooth/BluetoothOppManager.h
@@ -0,0 +1,54 @@
+/* -*- Mode: c++; c-basic-offset: 2; indent-tabs-mode: nil; tab-width: 40 -*- */
+/* vim: set ts=2 et sw=2 tw=80: */
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+#ifndef mozilla_dom_bluetooth_bluetoothoppmanager_h__
+#define mozilla_dom_bluetooth_bluetoothoppmanager_h__
+
+#include "BluetoothCommon.h"
+#include "mozilla/ipc/Socket.h"
+
+BEGIN_BLUETOOTH_NAMESPACE
+
+class BluetoothReplyRunnable;
+
+class BluetoothOppManager : public mozilla::ipc::SocketConsumer
+{
+public:
+  /*
+   * Channel of reserved services are fixed values, please check
+   * function add_reserved_service_records() in
+   * external/bluetooth/bluez/src/adapter.c for more information.
+   */
+  static const int DEFAULT_OPP_CHANNEL = 10;
+
+  ~BluetoothOppManager();
+  static BluetoothOppManager* Get();
+  void ReceiveSocketData(mozilla::ipc::SocketRawData* aMessage);
+
+  /*
+   * If a application wnats to send a file, first, it needs to
+   * call Connect() to create a valid RFCOMM connection. After
+   * that, call SendFile()/StopSendingFile() to control file-sharing
+   * process. At the end, the application will get a system message
+   * indicating that te process is complete, then it can either call
+   * Disconnect() to close RFCOMM connection or start another file-sending
+   * thread via calling SendFile() again.
+   */
+  bool Connect(const nsAString& aDeviceObjectPath,
+               BluetoothReplyRunnable* aRunnable);
+  bool Disconnect(BluetoothReplyRunnable* aRunnable);
+
+  bool SendFile(const nsAString& aFileUri,
+                BluetoothReplyRunnable* aRunnable);
+  bool StopSendingFile(BluetoothReplyRunnable* aRunnable);
+
+private:
+  BluetoothOppManager();
+};
+
+END_BLUETOOTH_NAMESPACE
+
+#endif
